9b2217afc9552fd5cef929896e44e3f89cdba674
DianArpian <>
2020-10-20 19:32:57 +0700

    Logika

---
+++ src/pbo06/pkg10119916/latihan10/logika/PBO0610119916Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916 
+ * Deskripsi Program : Logika
+ */
+public class PBO0610119916Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
